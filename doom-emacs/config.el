14a0bae9c02c7a9c98295b7d2ecd76173abc529f
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2022-10-08 22:46:05 +0300

    Add doom-emacs configuration

---
+++ doom-emacs/config.el
@@ -0,0 +1,81 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets.
+(setq user-full-name "Lukas Bloznelis"
+      user-mail-address "lukas@bluelabs.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
+;; are the three important ones:
+;;
+;; + `doom-font'
+;; + `doom-variable-pitch-font'
+;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;;
+;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
+;; font string. You generally only need these two:
+ (setq doom-font (font-spec :family "JetBrainsMono" :size 14 :weight 'light)
+       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'lukas-solarized-light)
+
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type 'relative)
+
+;; cider
+
+;; Here are some additional functions/macros that could help you configure Doom:
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+;; Modeline configuration
+(setq doom-modeline-buffer-file-name-style 'file-name)
+
+;; Treemacs icons
+(setq doom-themes-treemacs-theme "doom-colors")
+
+;; Lukas' settings
+(setq scroll-conservatively 1)
+
+' runnables
+(keychain-refresh-environment)
+
+;; Lukas' extensions
+(defun duplicate-line ()
+  "Duplicate current line"
+  (interactive)
+  (kill-whole-line)
+  (yank)
+  (yank))
+
+(map! "C-x C-d" #'duplicate-line)
+
+(require 'expand-region)
+(map! "C-x C-w" #'er/expand-region)
---
+++ doom-emacs/custom.el
@@ -0,0 +1,14 @@
+(put 'customize-face 'disabled nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files '("~/first.org"))
+ '(warning-suppress-types '((before-save-hook) (before-save-hook))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(shadow ((t (:foreground "AntiqueWhite4")))))
