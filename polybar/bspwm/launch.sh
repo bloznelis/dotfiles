d160b60ea6d2c614bb5ec8f664772253f85d0108
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2022-10-11 20:02:19 +0300

    Replace `/lukas/home` with `$HOME`

--- polybar/bspwm/launch.sh
+++ polybar/bspwm/launch.sh
@@ -7,8 +7,8 @@ killall -q polybar
 
 # Launch bars
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar -c /home/lukas/.config/polybar/bspwm/config.ini main >> /tmp/polybar.log 2>&1 &
-polybar -c /home/lukas/.config/polybar/bspwm/config.ini side-monitor >> /tmp/polybar-2.log 2>&1 &
+polybar -c $HOME/.config/polybar/bspwm/config.ini main >> /tmp/polybar.log 2>&1 &
+polybar -c $HOME/.config/polybar/bspwm/config.ini side-monitor >> /tmp/polybar-2.log 2>&1 &
 disown
 
 echo "Bars launched..."
