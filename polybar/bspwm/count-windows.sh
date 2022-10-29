e89889199d70559c8185f9bd1b224f67520acbf0
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2022-10-29 19:06:46 +0300

    Improve monocle expierience

---
+++ polybar/bspwm/count-windows.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# Counts BSPWM windows in monocle mode
+
+layout=$(bspc query -T -d | jq -r .layout)
+
+if [ $layout = "monocle" ]; then
+    window_count=$(bspc query -N -d $somedesktop -n .window.\!hidden | wc -l)
+    if [ $window_count -ne 0 ]; then
+        echo $window_count
+    else
+        echo ""
+    fi
+else
+    echo ""
+fi
