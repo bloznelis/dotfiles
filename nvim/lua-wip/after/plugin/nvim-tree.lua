a5c22291f07da8f8c1199269284e051a8d35374b
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2023-02-20 18:11:54 +0200

    Add nvim-tree

---
+++ nvim/lua-wip/after/plugin/nvim-tree.lua
@@ -0,0 +1,12 @@
+-- disable netrw at the very start of your init.lua (strongly advised)
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- empty setup using defaults
+require("nvim-tree").setup()
+
+vim.cmd('nnoremap <leader>op :NvimTreeToggle<CR>')
+vim.cmd('nnoremap <leader>oP :NvimTreeFindFile<CR>')
--- nvim/lua-wip/lua/core/packer.lua
+++ nvim/lua-wip/lua/core/packer.lua
@@ -2,47 +2,59 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-          'nvim-telescope/telescope.nvim', tag = '0.1.1',
-          -- or                            , branch = '0.1.x',
-          requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use 'folke/tokyonight.nvim'
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
 
-  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        }
+    }
 
-  use("mbbill/undotree")
+    use 'folke/tokyonight.nvim'
 
-  use("tpope/vim-fugitive")
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
 
-  use("preservim/nerdcommenter")
+    use("mbbill/undotree")
 
-  use("lewis6991/gitsigns.nvim")
+    use("tpope/vim-fugitive")
 
-  use {
-          'VonHeikemen/lsp-zero.nvim',
-          branch = 'v1.x',
-          requires = {
-                  -- LSP Support
-                  {'neovim/nvim-lspconfig'},             -- Required
---                  {'williamboman/mason.nvim'},           -- Optional
---                  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use("preservim/nerdcommenter")
 
-                  -- Autocompletion
-                  {'hrsh7th/nvim-cmp'},         -- Required
-                  {'hrsh7th/cmp-nvim-lsp'},     -- Required
-                  {'hrsh7th/cmp-buffer'},       -- Optional
-                  {'hrsh7th/cmp-path'},         -- Optional
-                  {'saadparwaiz1/cmp_luasnip'}, -- Optional
-                  {'hrsh7th/cmp-nvim-lua'},     -- Optional
+    use("lewis6991/gitsigns.nvim")
 
-                  -- Snippets
-                  {'L3MON4D3/LuaSnip'},             -- Required
-                  {'rafamadriz/friendly-snippets'}, -- Optional
-          }
-  }
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' }, -- Required
+            --                  {'williamboman/mason.nvim'},           -- Optional
+            --                  {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' }, -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'hrsh7th/cmp-buffer' }, -- Optional
+            { 'hrsh7th/cmp-path' }, -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' }, -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
+    }
 end)
