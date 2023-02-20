a8e3635758a91394f821688e7cf165afc98383d2
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2023-02-20 11:00:23 +0200

    Add some Fugitive binds

--- nvim/lua-wip/after/plugin/fugitive.lua
+++ nvim/lua-wip/after/plugin/fugitive.lua
@@ -1,1 +1,24 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+local lukas_fugitive = vim.api.nvim_create_augroup("lukas_fugitive", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = lukas_fugitive,
+    pattern = "*",
+    callback = function()
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "pp", function()
+            vim.cmd.Git('push')
+        end, opts)
+
+        vim.keymap.set("n", "P", function()
+            vim.cmd.Git({'pull',  '--rebase'})
+        end, opts)
+    end,
+})
