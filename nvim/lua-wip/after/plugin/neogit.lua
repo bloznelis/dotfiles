a2b37dd2ad705ac47fd058371488bf848ce709fa
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2023-03-08 10:50:57 +0200

    Remove fugitive

--- nvim/lua-wip/after/plugin/neogit.lua
+++ nvim/lua-wip/after/plugin/neogit.lua
@@ -1,1 +1,3 @@
 require('neogit').setup {}
+
+vim.keymap.set("n", "<leader>gg", vim.cmd.Neogit)
--- nvim/lua-wip/lua/core/packer.lua
+++ nvim/lua-wip/lua/core/packer.lua
@@ -45,7 +45,6 @@ return require('packer').startup(function(use)
     })
 
     -- git
-    use("tpope/vim-fugitive")
     use("lewis6991/gitsigns.nvim")
     use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
 
