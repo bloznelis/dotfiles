189ef3acd76b2a240d2b4e436eaf3e24a0eee96d
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2023-06-22 18:20:22 +0300

    Nvim config structure cleanup

--- nvim/lua-wip/lua/plugins/configuration/neogit.lua
+++ nvim/lua-wip/lua/plugins/configuration/neogit.lua
@@ -1,5 +1,6 @@
 return {
   "TimUntersberger/neogit",
+  keys = "<leader>gg",
   config = function()
     require('neogit').setup({ disable_commit_confirmation = true })
     vim.keymap.set("n", "<leader>gg", vim.cmd.Neogit)
--- nvim/lua-wip/lua/plugins/init.lua
+++ nvim/lua-wip/lua/plugins/init.lua
@@ -1,89 +1,14 @@
-return {
-  -- colors
-  --use 'folke/tokyonight.nvim'
-  --use 'AlexvZyl/nordic.nvim'
-  --use 'navarasu/onedark.nvim'
-  --use 'sainnhe/gruvbox-material'
-  --use 'shaunsingh/nord.nvim'
-  --use 'shaunsingh/solarized.nvim'
-  "rebelot/kanagawa.nvim",
-  --use "rose-pine/neovim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-  -- syntax
-  { "nvim-treesitter/nvim-treesitter",          build = ":TSUpdate" },
-  "elkowar/yuck.vim",
-  "NoahTheDuke/vim-just",
-
-  -- telescope --
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    -- or                            , branch = '0.1.x',
-    dependencies = { { 'nvim-lua/plenary.nvim' } }
-  },
-  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-  { 'nvim-telescope/telescope-project.nvim' },
-
-  -- icons
-  'nvim-tree/nvim-web-devicons',
-
-  -- lualine
-  {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons', opt = true }
-  },
-
-  -- clojure
-  'Olical/conjure',
-
-  -- nvim-tree
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',       -- optional, for file icons
-    }
-  },
-
-
-  -- surround
-  'echasnovski/mini.nvim',
-  "kylechui/nvim-surround",
-
-  -- auto closing brackets
-  "windwp/nvim-autopairs",
-
-  -- git
-  "lewis6991/gitsigns.nvim",
-  { 'TimUntersberger/neogit', dependencies = 'nvim-lua/plenary.nvim' },
-
-  -- misc
-  "folke/zen-mode.nvim",
-  "preservim/nerdcommenter",
-  "phaazon/hop.nvim",
-  "ThePrimeagen/harpoon",
-
-  -- LSP
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },       -- Required
-      --                  {'williamboman/mason.nvim'},           -- Optional
-      --                  {'williamboman/mason-lspconfig.nvim'}, -- Optional
-
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },               -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },           -- Required
-      { 'hrsh7th/cmp-buffer' },             -- Optional
-      { 'hrsh7th/cmp-path' },               -- Optional
-      { 'saadparwaiz1/cmp_luasnip' },       -- Optional
-      { 'hrsh7th/cmp-nvim-lua' },           -- Optional
-
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },                   -- Required
-      { 'rafamadriz/friendly-snippets' },       -- Optional
-    }
-  },
-  { 'scalameta/nvim-metals',  dependencies = { "nvim-lua/plenary.nvim" } },
-}
+require("lazy").setup("plugins.configuration")
