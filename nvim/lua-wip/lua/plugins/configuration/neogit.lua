7cbab053fe923ebb2f44deb24f9a18ab31fce641
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2023-07-15 10:49:55 +0300

    Use magit style keybindings for neogit

--- nvim/lua-wip/lua/plugins/configuration/neogit.lua
+++ nvim/lua-wip/lua/plugins/configuration/neogit.lua
@@ -3,7 +3,10 @@ return {
   keys = "<leader>gg",
   dependencies = 'nvim-lua/plenary.nvim' ,
   config = function()
-    require('neogit').setup({ disable_commit_confirmation = true })
+    require('neogit').setup({
+      disable_commit_confirmation = true,
+      use_magit_keybindings = true
+    })
     vim.keymap.set("n", "<leader>gg", vim.cmd.Neogit)
   end
 }
