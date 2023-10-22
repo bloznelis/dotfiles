dad4996bb28990e7d84bd81971abc0fa00b48385
Lukas Bloznelis <lukas.bloznelis@gmail.com>
2023-10-22 14:27:38 +0300

    Clojure DX improvements

--- nvim/lua/plugins/configuration/conjure.lua
+++ nvim/lua/plugins/configuration/conjure.lua
@@ -1,4 +1,28 @@
 return {
   "Olical/conjure",
-  ft = "clojure"
+  branch = "develop",
+  ft = { "clojure" },
+  config = function()
+    require("conjure.main").main()
+    require("conjure.mapping")["on-filetype"]()
+  end,
+  init = function()
+    vim.g["conjure#log#hud#width"] = 1
+    vim.g["conjure#log#hud#enabled"] = true
+    vim.g["conjure#log#hud#anchor"] = "SE"
+    vim.g["conjure#log#botright"] = true
+    vim.g["conjure#extract#context_header_lines"] = 100
+    vim.g["conjure#eval#comment_prefix"] = ";; "
+
+    vim.g["conjure#mapping#enable_defaults"] = false -- disables default Conjure bindings
+
+    vim.g["conjure#mapping#eval_current_form"] = "ee"
+    vim.g["conjure#mapping#eval_root_form"] = "er"
+    vim.g["conjure#mapping#eval_comment_root_form"] = "e;"
+    vim.g["conjure#mapping#eval_buf"] = "eb"
+    vim.g["conjure#mapping#eval_visual"] = "E"
+
+    vim.g["conjure#mapping#log_toggle"] = "el"
+    vim.g["conjure#mapping#doc_word"] = ""
+  end,
 }
